0b7a0b1822ff2a813a43cbee619b4f048f08b41a
Eric Cifuentes <eaifue@gmail.com>
2023-01-18 02:52:36 -0500

    add login page and login funcionality, using the api and db in railway

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import LoginComponent from './componets/Login.js'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LoginComponent from './componets/Login/Login.js'
 
 function App() {
   return (
     <div className="App">
-      <LoginComponent/>
+    <Router>
+      <div className="App">
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+            <Route exact path="/" element={<LoginComponent />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
     </div>
   );
 }
